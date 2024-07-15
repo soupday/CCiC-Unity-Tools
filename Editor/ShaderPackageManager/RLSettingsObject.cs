3e6843806d2bc77f857f5d4dda5d4b17d8b23720
soupday <79094830+soupday@users.noreply.github.com>
2024-07-15 15:50:35 +0100

    2.0.0
    -

---
+++ Editor/ShaderPackageManager/RLSettingsObject.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Reallusion.Import
+{
+    public class RLSettingsObject : ScriptableObject
+    {
+        public bool showOnStartup;
+        public string lastPath;
+    }
+}
